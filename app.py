a3aae915d03f1f692ec5138b2e3b63af90bce9b9
crapczynski <crapczynski@gmail.com>
2026-02-03 20:54:45 -0500

    I added "This is my first HTML page"

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World from Robert Smith!'
+    return 'Hello World from Robert Smith! This is my first HTML page.'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="127.0.0.1", port=5001, debug=True)
