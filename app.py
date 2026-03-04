e774e5827079c3daf84a8ce2fa4e645df3427cc2
crapczynski <crapczynski@gmail.com>
2026-03-03 20:24:27 -0500

    I am routing about-css.

--- app.py
+++ app.py
@@ -15,5 +15,9 @@ def hello():
 def about():
     return render_template('about.html')
 
+@app.route('/about-css')
+def about():
+    return render_template('about-css.html')
+
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=5001, debug=True)
